34a5768ab542035453e8d14dba376d49d3f1ded1
Mukund Kale <mukund1997kale@gmail.com>
2023-09-05 00:44:46 +0530

    "auth working"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Components/Home';
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import Checkout from './Checkout';
+import Login from './Login';
 
 
 
@@ -12,6 +13,7 @@ function App() {
         <div className="App">
            <Header />
            <Routes>
+           <Route path="/login" element={<Login />} />
              <Route path="/" element={<Home />} />
              <Route path="/checkout" element={<Checkout />} />
            </Routes>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDeFBCc5T-NBftOlsTF9ADHsm8655KU8oA",
   authDomain: "clone-af818.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:399175561909:web:4af111ffacea25bd2344fc",
   measurementId: "G-G685KPF004"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
